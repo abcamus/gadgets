914b3181f546b9a1b66dbde401ac40914df9ddc8
kqiu <kai.qiu@artosyn.com>
2017-04-18 13:46:50 +0800

    1. update README 2. add life caculator

--- README.md
+++ README.md
@@ -1,2 +1,7 @@
 # gadgets
 some interesting programs
+
+## Content Lists:
+
+1. a note editor based on python
+2. a Life Calculator
---
+++ life-calc/day_left.c
@@ -0,0 +1,119 @@
+#include <stdio.h>
+#include <time.h>
+#include <malloc.h>
+#include <stdlib.h>
+
+struct Cal_t {
+	int cur_year;
+    int day_cnt;
+    int week_cnt;
+    int month_cnt;
+};
+
+int leap_year[12] = {31, 29, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31};
+int normal_year[12] = {31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31};
+
+struct Cal_t *calendar;
+
+int add_days(int month_day[], int month)
+{
+    int sum_days = 0, idx;
+    for (idx = 0; idx < month; idx += 1)
+        sum_days += month_day[idx];
+
+    return sum_days;
+}
+
+void genCalendar(struct tm *p)
+{
+    // generate Calendar for 100 years from current year
+    int idx = 0;
+    
+    calendar = malloc(sizeof(struct Cal_t)*100);
+
+    if (NULL == calendar) {
+        printf ("No enough mem for Calendar\n");
+        exit(-1);
+    }
+
+    for (idx = 0; idx < 100; idx += 1) {
+        int year = 1900 + p->tm_year+idx;
+		calendar[idx].cur_year = year;
+        if (0 == idx) {
+            calendar[idx].month_cnt = 12-p->tm_mon;
+            if (0 == year%4)
+                calendar[idx].day_cnt = 366-add_days(leap_year, 1+p->tm_mon);
+            else
+                calendar[idx].day_cnt = 365-add_days(normal_year, 1+p->tm_mon);
+            calendar[idx].day_cnt += p->tm_mday;
+            calendar[idx].month_cnt = 12-(1+p->tm_mon);
+        }
+        else {
+            if (0 == year%4) {
+                calendar[idx].day_cnt = 366;
+            }
+            else
+                calendar[idx].day_cnt = 365;
+            calendar[idx].month_cnt = 12;
+        }
+
+        calendar[idx].week_cnt = calendar[idx].day_cnt/7;
+    }
+}
+
+int calculateDay(int age)
+{
+#define END_OF_AGE 80
+    int year_cnt = END_OF_AGE - age;
+    int sum_days = 0, idx;
+
+    for (idx = 0; idx < year_cnt; idx += 1)
+        sum_days += calendar[idx].day_cnt;
+
+    return sum_days;
+}
+
+void show_detail(int age)
+{
+	int idx;
+
+	for (idx = age; idx < END_OF_AGE; idx += 1) {
+		printf("Year/Age:\tmonths\tweeks\tdays\n");
+		printf("%d/%d:\t%d\t%d\t%d\n", calendar[idx-age].cur_year, idx, calendar[idx-age].month_cnt, calendar[idx-age].week_cnt, calendar[idx-age].day_cnt);
+	}
+}
+
+void clear()
+{
+    if (calendar)
+        free(calendar);
+}
+
+int main(int argc, char *argv[])
+{
+	/*
+	 * Calculate your day left in this world
+	 */
+	int age, dayleft;
+	time_t timep;
+	struct tm *p;
+	printf("Enter Your Age: ");
+	scanf("%d", &age);
+
+	//printf("Your age = %d.\n", age);
+	time(&timep);
+	p = gmtime(&timep);
+	//printf("%d:%d:%d\n", 1900+p->tm_year, 1+p->tm_mon, p->tm_mday);
+	//year = 1900+p->tm_year;
+	//month = 1+p->tm_mon;
+	//day = p->tm_mday;
+    genCalendar(p);
+
+	dayleft = calculateDay(age);
+    printf ("Your Days Left = %d\n", dayleft);
+	show_detail(age);
+
+    clear();
+
+	return 0;
+}
